011f15dc3d51a86af5ac04584df574aa483c5c55
Dalton Rogerio Alvarez <44284851+rogeriodalton@users.noreply.github.com>
2020-11-16 16:12:59 -0300

    Add files via upload

--- apache_public/mariabonita/index.php
+++ apache_public/mariabonita/index.php
@@ -1,43 +1,30 @@
 <?php
 ini_set('default_charset', 'utf-8'); 
 
-$varSystem = require_once '/mariabonita/Source/route/vars.php';
+$varSystem = require_once '/home/storage/6/0b/e6/mariabonitaservi1/cfg/Source/route/vars.php';
 
 $the_request = [];
 $RequestMethod = '';
 if (array_key_exists('REQUEST_METHOD', $_SERVER)==true)
 	$RequestMethod = $_SERVER['REQUEST_METHOD'];
-
+$url = (string)'';
 switch($RequestMethod){
 case 'GET':	$the_request = &$_GET;
-			break;
+            $url = &$the_request['url'];			
+            break;
 			
 case 'POST': $the_request = &$_POST;
+             $url = $_REQUEST['url'];
 			 break; 
 }
 
-$EhHome = (empty($the_request)); 
-if ($EhHome){
+if ($url=='')
 	require_once $varSystem['Home'];  
-	die();
-} else
-if ($RequestMethod == 'GET'){
-  $url = &$the_request['url'];
-} else
-if ($RequestMethod == 'POST'){
-  $url = $_REQUEST['url'];
-}
-
-// Identifica URL e abre caso exista no arquivo de rotas ---// 
-// em UNIX e Linux  /mariabonita/Source/route/vars.php      //
-// em Windows     C:\mariabonita\Source\route\vars.php      //
-//----------------------------------------------------------//
-if (array_key_exists($url, $varSystem))
-{
+else 
+if (array_key_exists($url, $varSystem)){
 	$arquivo = $varSystem[$url];
 	if (!(file_exists($arquivo)))
 		die('<h1>' . $url . '  não encontrado </h1>');
-
 	require_once $arquivo;
 }	
 else
