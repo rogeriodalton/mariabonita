c4281284d7e7eb896bdbee736a089ac12a4e3710
Dalton Rogerio Alvarez <44284851+rogeriodalton@users.noreply.github.com>
2020-11-16 15:10:33 -0300

    Update vars.php

--- Source/route/vars.php
+++ Source/route/vars.php
@@ -1,9 +1,9 @@
 <?php
 
-return ["Home"              => "/mariabonita/Source/View/home.php",
-        "NotFound"          => "/mariabonita/Source/View/PageNotFound.php",
-        "sobre"             => "/mariabonita/Source/View/About.php",
-        "ListarClientes"    => "/mariabonita/Source/View/ListarClientes.php",
+return ["Home"              => "/mariabonita/Source/view/home.php",
+        "NotFound"          => "/mariabonita/Source/view/PageNotFound.php",
+        "sobre"             => "/mariabonita/Source/view/About.php",
+        "ListarClientes"    => "/mariabonita/Source/view/ListarClientes.php",
         "GravaCliente"      => "/mariabonita/Source/app/ClienteGrava.php",
         "CadastroCliente"   => "/mariabonita/Source/view/CadastroCliente.php",
         "ClienteExcluir"    => "/mariabonita/Source/app/ClienteExcluir.php"
