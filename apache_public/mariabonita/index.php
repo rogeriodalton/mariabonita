1d2a93db5b0adb2ecfcf6b48d79b0597575f2dee
Dalton Rogerio Alvarez <44284851+rogeriodalton@users.noreply.github.com>
2020-11-16 16:34:26 -0300

    Update index.php

--- apache_public/mariabonita/index.php
+++ apache_public/mariabonita/index.php
@@ -6,27 +6,27 @@ $varSystem = require_once '/mariabonita/Source/route/vars.php';
 $the_request = [];
 $RequestMethod = '';
 if (array_key_exists('REQUEST_METHOD', $_SERVER)==true)
-	$RequestMethod = $_SERVER['REQUEST_METHOD'];
+    $RequestMethod = $_SERVER['REQUEST_METHOD'];
 	
 $url = (string)'';
 switch($RequestMethod){
-case 'GET':	$the_request = &$_GET;
-            $url = &$the_request['url'];			
-            break;
+case  'GET': $the_request = &$_GET;
+             $url = &$the_request['url'];			
+             break;
 			
 case 'POST': $the_request = &$_POST;
              $url = $_REQUEST['url'];
-			 break; 
+	     break; 
 }
 
 if ($url=='')
 	require_once $varSystem['Home'];  
 else 
 if (array_key_exists($url, $varSystem)){
-	$arquivo = $varSystem[$url];
-	if (!(file_exists($arquivo)))
-		die('o caminho solicitado "' . $url . '" existe no entanto o arquivo não foi encontrado no servidor');
-	require_once $arquivo;
+    $arquivo = $varSystem[$url];
+if (!(file_exists($arquivo)))
+    die('o caminho solicitado "' . $url . '" existe no entanto o arquivo não foi encontrado no servidor');
+    require_once $arquivo;
 }	
 else
   require_once $varSystem['NotFound'];
