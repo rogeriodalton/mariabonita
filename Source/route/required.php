61b08193a3fcd6ea8286c09fae7bfa7790b3077c
Dalton Rogerio Alvarez <44284851+rogeriodalton@users.noreply.github.com>
2020-11-16 16:19:00 -0300

    Update required.php

--- Source/route/required.php
+++ Source/route/required.php
@@ -1,12 +1,12 @@
 <?php
 
-return ["Database"         => "/mariabonita/Source/dba/dbConn.php",
+return ["Database"            => "/mariabonita/Source/dba/dbConn.php",
         "classNewUpdCliente"  => "/mariabonita/Source/dba/classNewUpdCliente.php",
-        "classDelCliente"  => "/mariabonita/Source/dba/classDelCliente.php",
-        "classSelCliente"  => "/mariabonita/Source/dba/classSelCliente.php",
-        "fnExibir"         => "/mariabonita/Source/app/fnExibir.php",
-        "fnTable"          => "/mariabonita/Source/app/fnTable.php",
-        "fnCEP"            => "/mariabonita/Source/app/fnCEP.php",
-	"fnUrlReferencia"  => "/mariabonita/Source/app/fnUrlReferencia.php"
+        "classDelCliente"     => "/mariabonita/Source/dba/classDelCliente.php",
+        "classSelCliente"     => "/mariabonita/Source/dba/classSelCliente.php",
+        "fnExibir"            => "/mariabonita/Source/app/fnExibir.php",
+        "fnTable"             => "/mariabonita/Source/app/fnTable.php",
+        "fnCEP"               => "/mariabonita/Source/app/fnCEP.php",
+	"fnUrlReferencia"     => "/mariabonita/Source/app/fnUrlReferencia.php"
         ]
 ?>
