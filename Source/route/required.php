8f8fca5dfc2e3b3a8463855d67a75b1fc4d64e33
Dalton Rogerio Alvarez <44284851+rogeriodalton@users.noreply.github.com>
2020-11-16 23:43:31 -0300

    Add files via upload

--- Source/route/required.php
+++ Source/route/required.php
@@ -1,12 +1,13 @@
 <?php
 
-return ["Database"            => "/mariabonita/Source/dba/dbConn.php",
-        "classNewUpdCliente"  => "/mariabonita/Source/dba/classNewUpdCliente.php",
-        "classDelCliente"     => "/mariabonita/Source/dba/classDelCliente.php",
-        "classSelCliente"     => "/mariabonita/Source/dba/classSelCliente.php",
-        "fnExibir"            => "/mariabonita/Source/app/fnExibir.php",
-        "fnTable"             => "/mariabonita/Source/app/fnTable.php",
-        "fnCEP"               => "/mariabonita/Source/app/fnCEP.php",
-	"fnUrlReferencia"     => "/mariabonita/Source/app/fnUrlReferencia.php"
+return ["Database"              => "/mariabonita/Source/dba/dbConn.php",
+        "classNewUpdCliente"    => "/mariabonita/Source/dba/classNewUpdCliente.php",
+        "classDelCliente"       => "/mariabonita/Source/dba/classDelCliente.php",
+        "classSelCliente"       => "/mariabonita/Source/dba/classSelCliente.php",
+        "fnExibir"              => "/mariabonita/Source/app/fnExibir.php",
+        "fnTable"               => "/mariabonita/Source/app/fnTable.php",
+        "fnCEP"                 => "/mariabonita/Source/app/fnCEP.php",
+        "fnUrlReferencia"       => "/mariabonita/Source/app/fnUrlReferencia.php",
+        "fnSubDominioAnterior"  => "/mariabonita/Source/app/fnSubDominioAnterior.php"
         ]
 ?>
