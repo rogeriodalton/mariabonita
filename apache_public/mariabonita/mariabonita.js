513c186e5f805fb535a9e5e1dab6e35871762b71
Dalton Rogerio Alvarez <44284851+rogeriodalton@users.noreply.github.com>
2020-11-17 22:33:57 -0300

    Add files via upload

--- apache_public/mariabonita/mariabonita.js
+++ apache_public/mariabonita/mariabonita.js
@@ -30,3 +30,36 @@ function ListarClientes(){
             }
     });
 }
+
+function ConsultaCEP(CEP){
+      $.ajax 
+      ({
+              method: 'GET',    
+              type: 'GET',
+              dataType: 'json',
+              url: `https://viacep.com.br/ws/${CEP}/json/`,
+              data: {CEP : CEP},
+              beforeSend: function () {
+                  $("#cidade").val("Aguarde ...");
+                  $("#uf").val("");
+                  $("#bairro").val("Aguarde ...");
+                  $("#rua").val("Aguarde ...");
+                  $("#complemento").val("Aguarde ...");
+              },              
+              success: function (msg){
+                  $("#cidade").val(msg.localidade);
+                  $("#uf").val(msg.uf);
+                  $("#bairro").val(msg.bairro);
+                  $("#rua").val(msg.logradouro);
+                  $("#complemento").val(msg.complemento);
+              },
+              error : function(erro) {
+                  $("#cidade").val("");
+                  $("#uf").val("");
+                  $("#bairro").val("");
+                  $("#rua").val("");
+                  $("#complemento").val("");
+                  alert("CEP não encontrado.");
+              } 
+      });
+}
