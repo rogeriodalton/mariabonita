85cd9ec71394d12de0edc1bebac3bb7ade29173d
Dalton Rogerio Alvarez <44284851+rogeriodalton@users.noreply.github.com>
2020-11-29 14:24:30 -0300

    Add files via upload

--- apache_public/mariabonita/index.php
+++ apache_public/mariabonita/index.php
@@ -1,35 +1,42 @@
 <?php
 ini_set('default_charset', 'utf-8'); 
+$minPHPVersion = '7.3';
+$PHP_Version = phpversion();
+if ($PHP_Version < $minPHPVersion)
+	die("Esse website requer PHP {$minPHPVersion} ou superior. <br> Versão atual: {$PHP_Version}");
 
-$varSystem = require_once '/mariabonita/Source/route/vars.php';
-$required  = require_once '/mariabonita/Source/route/required.php';
+//$SP = '/home/storage/6/0b/e6/mariabonitaservi1/cfg/Source';
+$SP = '/mariabonita/Source';
+define ('varSystem', require_once "{$SP}/route/vars.php");
+define ('required',  require_once "{$SP}/route/required.php");
 
-$the_request = [];
 $RequestMethod = '';
 if (array_key_exists('REQUEST_METHOD', $_SERVER)==true)
     $RequestMethod = $_SERVER['REQUEST_METHOD'];
 	
-$url = (string)'';
 switch($RequestMethod){
-    case  'GET': $the_request = &$_GET;
-                 $url = &$the_request['url'];			
+    case  'GET': if (array_key_exists('url', $_GET)){
+                   define('the_request', $_GET);
+                   define('url', the_request['url']);
+                 }  
                  break;
 		
-    case 'POST': $the_request = &$_POST;
-                 $url = $_REQUEST['url'];
+    case 'POST': $_POST['url'] = $_REQUEST['url'];
+                 define('the_request', $_POST);
+                 define('url', the_request['url']);
 	         break; 
 }
 
-if ($url=='')
-    require_once $varSystem['Home'];  
+if (!defined('url'))
+    require_once varSystem['Home'];  
 else 
-if (array_key_exists($url, $varSystem)){
-    $arquivo = $varSystem[$url];
-if (!(file_exists($arquivo)))
-    die("o caminho solicitado {$url} existe no entanto o arquivo não foi encontrado no servidor");
-    require_once $arquivo;
+if (array_key_exists(url, varSystem)){
+    define('arquivo', varSystem[url]);
+if (!(file_exists(arquivo)))
+	die("o caminho solicitado " . url . " existe no entanto o arquivo não foi encontrado no servidor");
+    require_once arquivo;
 }	
 else
-  require_once $varSystem['NotFound'];
+  require_once varSystem['NotFound'];
 	
 ?>
