ea00314191f57789cc933a52a3675c23700d1d52
Dalton Rogerio Alvarez <44284851+rogeriodalton@users.noreply.github.com>
2020-11-29 15:14:37 -0300

    Update fnSubDominioAnterior.php

--- Source/app/fnSubDominioAnterior.php
+++ Source/app/fnSubDominioAnterior.php
@@ -3,7 +3,7 @@ function fnSubDominioAnterior(){
     if ($_SERVER['HTTP_HOST']=='localhost')
         $EnderecoCompleto = required[$_SERVER['HTTP_HOST']];
     else
-        $EnderecoCompleto ="https://{$_SERVER['HTTP_HOST']}";
+        $EnderecoCompleto = "https://{$_SERVER['HTTP_HOST']}";
     
     echo "<a href=\"{$EnderecoCompleto}\"><b>Voltar</b></a>";
       
